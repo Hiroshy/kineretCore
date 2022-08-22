00c19d708495f6bef65b939d0528d8c52c04f912
Hiroshy Perez Diaz <hiroshyperez@wolfsellers.com>
2022-08-22 00:15:52 -0500

    Remove path absolute

--- parts/head/link.php
+++ parts/head/link.php
@@ -1,19 +1,19 @@
     <!-- Custom font-->
     <link rel="stylesheet" href="fonts/stylesheet.e9dc714d.css">
     <!-- Swiper-->
-    <link rel="stylesheet" href="/vendor/swiper/css/swiper.min.css">
+    <link rel="stylesheet" href="vendor/swiper/css/swiper.min.css">
     <!-- AOS - AnimationOnScroll-->
-    <link rel="stylesheet" href="/vendor/aos/aos.css">
+    <link rel="stylesheet" href="vendor/aos/aos.css">
     <!-- Custom Scrollbar-->
-    <link rel="stylesheet" href="/vendor/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css">
+    <link rel="stylesheet" href="vendor/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css">
     <!-- Theme stylesheet-->
-    <link rel="stylesheet" href="/css/style.default.d1b89a2d.css" id="theme-stylesheet">
+    <link rel="stylesheet" href="css/style.default.d1b89a2d.css" id="theme-stylesheet">
     <!-- Custom stylesheet - for your changes-->
-    <link rel="stylesheet" href="/css/custom.0a822280.css">
+    <link rel="stylesheet" href="css/custom.0a822280.css">
     <!-- Favicon-->
-    <link rel="icon" href="/img/app/inladsac_ico.jpg" sizes="192x192" />
-    <link rel="apple-touch-icon" href="/img/app/inladsac_ico.jpg" />
-    <link rel="shortcut icon" href="/img/app/inladsac_ico.jpg">
+    <link rel="icon" href="img/app/inladsac_ico.jpg" sizes="192x192" />
+    <link rel="apple-touch-icon" href="img/app/inladsac_ico.jpg" />
+    <link rel="shortcut icon" href="img/app/inladsac_ico.jpg">
     <!-- FontAwesome-->
     <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.10.0/css/all.css">
     <!-- ShortLink-->
